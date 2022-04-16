c8b3b394616c78b31091147fe56343fd93f34d88
MASUD-01 <masudrana96141@gmail.com>
2022-04-16 16:47:54 +0600

    LaunchItems create

---
+++ src/components/Food/Launch/Launch.js
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import { MyFoodContext } from "../../../App";
+import Header from "../../Header/Header";
+import NavPage from "../../Navbar/NavPage";
+import LunchItem from "./LunchItem/LunchItem";
+
+const Lunch = () => {
+    const [breakfast] = useContext(MyFoodContext);
+    return (
+        <div>
+            <NavPage />
+            <Header />
+            <div className="grid">
+                {breakfast.map((b) => (
+                    <LunchItem b={b} key={b.id} />
+                ))}
+            </div>
+            <div className="d-flex justify-content-center mb-5 mt-5">
+                <button className="border-0 py-1 px-2 rounded-3 bg-secondary text-white">Checkout Your Food</button>
+            </div>
+        </div>
+    );
+};
+
+export default Lunch;
---
+++ src/components/Food/Launch/LaunchItem/LaunchItem.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const LunchItem = ({ b }) => {
+    const { name, price, description, img } = b;
+    const food = b.time;
+    if (food === 'lunch') {
+        return (
+            <div className="meal py-4 px-1 m-3">
+                <img className="w-75" src={img} alt="" />
+                <h6>{name}</h6>
+                <p>
+                    <small className="text-secondary">{description.slice(0, 25)}</small>
+                </p>
+                <h5>$ {price}</h5>
+            </div>
+
+        );
+    };
+};
+
+export default LunchItem;
